4f023b539c809bec0e2546c1b677d25217e66ff3
Ezequiel <eze5025@gmail.com>
2025-06-22 02:46:08 -0300

    inicializados environments, removidos packages de testing y creado componente stickies

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,10 @@ export const routes: Routes = [
     path: 'table',
     loadComponent: () => import('./features/table-page/table-page.component')
   },
-  // {
-  //   path: 'sticky',
-  //   loadComponent: () => import('./features/sticky-page/sticky-page.component')
-  // },
+  {
+    path: 'sticky',
+    loadComponent: () => import('./features/stickies/stickies.component').then(m => m.StickiesComponent)
+  },
   {
     path: '**',
     redirectTo: 'table',
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+const API_BASE_URL = "http://127.0.0.1:8080";
+export const environment = {
+    API_BASE_URL: API_BASE_URL,
+    API_STICKIES_URL: `${API_BASE_URL}/stickies`
+};
